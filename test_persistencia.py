fd0fb7d4200ae5b68652eac3e28858c6d3fe1516
Salvap4 <perezluquesalva4@gmail.com>
2023-12-17 19:43:22 +0100

    Creación de test_persistencia

---
+++ test_persistencia.py
@@ -0,0 +1,19 @@
+"""
+Pruebas Persistencia
+"""
+import persistencia
+
+def test_guardar_pedido():
+    """
+    Prueba general
+    """
+    with open("pedidos.txt", "w+", encoding="utf-8") as file:
+        persistencia.guardar_pedido("Salvador", "Pérez Luque")
+        persistencia.guardar_pedido("Michael", "Jordan")
+        firstline = file.readline()
+        secondline = file.readline()
+        file.close()
+    assert firstline == "-Salvador Pérez Luque\n"
+    assert secondline == "-Michael Jordan\n"
+    
+test_guardar_pedido()
